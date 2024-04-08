8b3ef59cc9515394789694743c9676ae78fc4c99
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-04-08 12:44:06 +0530

    code added

--- app.js
+++ app.js
@@ -29,17 +29,26 @@
 
 // console.log("hello......");
 
-h1 = document.querySelector("h1");
+// h1 = document.querySelector("h1");
 
-function changeColor(color, delay, nextColorChange) {
-    setTimeout( ()=> {
-        h1.style.color = color;
-        if(nextColorChange) nextColorChange();
-    }, delay);
+// function changeColor(color, delay, nextColorChange) {
+//     setTimeout( ()=> {
+//         h1.style.color = color;
+//         if(nextColorChange) nextColorChange();
+//     }, delay);
+// }
+
+// changeColor("greenYellow", 1000, ()=> {
+//     changeColor("gray", 1000, ()=> {
+//         changeColor("yellow", 1000);
+//     });
+// });
+
+function savetoDb (data, success, failure) {
+    let internetSpeed = Math.floor( Math.random () * 10 ) + 1;
+    if(internetSpeed > 4 ) {
+        success();
+    } else {
+        failure;
+    }
 }
-
-changeColor("greenYellow", 1000, ()=> {
-    changeColor("gray", 1000, ()=> {
-        changeColor("yellow", 1000);
-    });
-});
