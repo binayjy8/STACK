867c428eabf16bec25e80c7991ed8beae65ae2d1
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-04-10 19:37:16 +0530

    code added

--- app.js
+++ app.js
@@ -29,16 +29,36 @@
 
 // console.log("hello......");
 
-//  h1 = document.querySelector("h1");
+ h1 = document.querySelector("h1");
 
-// function changeColor(color, delay) {
-//     return new Promise((resolve, reject)=>{
-//         setTimeout(()=> {
-//             h1.style.color = color;
-//             resolve("color changed");
-//         }, delay);
-//     });
-// }
+function changeColor(color, delay) {
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=> {
+            let num = Math.floor(Math.random() * 5 ) + 1;
+            if(num > 2) {
+                reject("promised rejected");
+            }
+
+            h1.style.color = color;
+            console.log(`color changed to ${color}!`);
+            resolve("color changed");
+        }, delay);
+    });
+}
+
+async function hell() {
+   try{
+    await changeColor("red", 1000);
+    await changeColor("gray", 1000);
+    await changeColor("yellow", 1000);
+    await changeColor("green", 1000);
+    await changeColor("blue", 1000);
+   } catch (err) {
+    console.log("err cought");
+    console.log(err);
+   }
+
+}
 
 // changeColor("red", 1000)
 // .then(()=>{
