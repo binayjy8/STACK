76b757065272e2f24b0ea0bcd539bd53483174bf
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-04-10 10:25:27 +0530

    code added

--- app.js
+++ app.js
@@ -70,6 +70,10 @@
 function savetoDb (data, success, failure) {
     return new Promise( (resolve, reject)=>{
         let internetSpeed = Math.floor(Math.random() * 10) + 1;
-        if(internetSpeed > 4) {}
+        if(internetSpeed > 4) {
+            resolve();
+        } else {
+            reject();
+        }
     });
 }
